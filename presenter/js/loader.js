6d1c44d5314e0fd9f09298a6e24f7aaec4a956d1
Tomáš Mlynarič <mlynarict@gmail.com>
2016-03-15 20:24:00 +0100

    Added testings

--- presenter/js/loader.js
+++ presenter/js/loader.js
@@ -2,41 +2,68 @@
  * @author Tomas Mlynaric
  */
 
-var server_window = log.server.window;
-var chro = [];
-for (var x = 0; x < server_window.data.length; x++) {
-    chro.push(x);
-}
-
-var data = {
-    labels: chro,
-    //labels: server_window.time,
-    datasets: [
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: server_window.data
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: log.client.window.data
-        }
-    ]
+var options = {
+    title: {
+        text: '',
+    },
+    xAxis: {
+        type: 'datetime',
+        units: [
+            [
+                'millisecond'
+            ]
+        ]
+    },
+    yAxis: {
+        //title: {
+        //    text: 'Temperature (°C)'
+        //},
+        plotLines: [{
+            value: 0,
+            width: 1,
+            color: '#808080'
+        }]
+    },
+    //tooltip: {
+    //    valueSuffix: '°C'
+    //},
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle',
+        borderWidth: 0
+    },
 };
 
 
-var ctx = $("#myChart").get(0).getContext("2d");
-var myLineChart = new Chart(ctx).Line(data, {
-    responsive: true
+$.getJSON('log.json', function (json) {
+    options.series = [
+        {
+            name: 'Server window',
+            data: json.server.windows
+        },
+        //{
+        //    name: 'Client window',
+        //    data: json.client.window
+        //}
+    ]
+
+    $('#container').highcharts(options);
 });
+
+//data: [
+//    //[Date.UTC(2010, 0, 1,11,11,12, 1), 29.9],
+//    //[Date.UTC(2010, 0, 1,11,11,12, 12), 71.5],
+//    //[Date.UTC(2010, 0, 1,11,11,12, 13), 106.4],
+//    //[Date.UTC(2010, 0, 1,11,11,12, 14), 129.2],
+//    //[Date.UTC(2010, 0, 1,11,11,12, 15), 144.0],
+//
+//    [1110033190.9, 50],
+//    [1110033190.9, 51],
+//    [1110033190.9, 52],
+//    [1110033191.07, 53],
+//    [1110033191.15, 54],
+//    [1110033191.23, 55],
+//    [1110033191.26, 56],
+//    [1110033191.26, 57],
+//]
